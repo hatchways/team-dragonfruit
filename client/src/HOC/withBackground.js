bcd6814064ecc2a983aa5d3c73ed3a5378fc4e54
samir <samir_sharif@yahoo.com>
2020-06-30 15:47:22 -0400

    Background HOC was added

---
+++ client/src/HOC/withBackground.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const style={
+    display:"flex",backgroundColor:"#6E3ADB",backgroundImage:"linear-gradient(#6E3ADB, #501CBD)",
+    alignItems: 'center',justifyContent: 'center', width:"100%",height:"100vh"
+}
+const withBackground = props => (
+    <div style={style}>
+        {props.children}
+    </div>
+)
+export default withBackground;
