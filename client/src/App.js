6956b100047a4e471af25b478327693cbd92d00f
Donadonzi <donadonzi@gmail.com>
2020-07-18 20:24:39 -0400

    clean up on editor files

--- client/src/App.js
+++ client/src/App.js
@@ -16,25 +16,23 @@ import OnboardingPage from "./pages/OnboardingPage";
 import "./App.css";
 
 function App() {
-
-  return (
-    <MuiThemeProvider theme={theme}>
-      <AuthProvider>
-        <BrowserRouter>
-          <Switch>
-            <PublicRoute path='/signup' component={Signup} />
-            <PublicRoute path='/login' component={Login} />
-            <PrivateRoute path='/' exact component={Profile} />
-            <PrivateRoute path='/balance' component={Balance} />
-            <PrivateRoute path='/checkout' component={Checkout} />
-            <PrivateRoute path='/reviews' exact component={Reviews} />
-            <PrivateRoute path='/onboarding' exact component={OnboardingPage} />
-          </Switch>
-        </BrowserRouter>
-      </AuthProvider>
-    </MuiThemeProvider>
-  );
-
+	return (
+		<MuiThemeProvider theme={theme}>
+			<AuthProvider>
+				<BrowserRouter>
+					<Switch>
+						<PublicRoute path="/signup" component={Signup} />
+						<PublicRoute path="/login" component={Login} />
+						<PrivateRoute path="/" exact component={Profile} />
+						<PrivateRoute path="/balance" component={Balance} />
+						<PrivateRoute path="/checkout" component={Checkout} />
+						<PrivateRoute path="/reviews" exact component={Reviews} />
+						<PrivateRoute path="/onboarding" exact component={OnboardingPage} />
+					</Switch>
+				</BrowserRouter>
+			</AuthProvider>
+		</MuiThemeProvider>
+	);
 }
 
 export default App;
