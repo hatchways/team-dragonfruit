195842486df36676b70de905bc89dcb6cdeb8b3f
Donadonzi <donadonzi@gmail.com>
2020-07-20 11:06:53 -0400

    modified style

--- client/src/components/LanguageRenderer.js
+++ client/src/components/LanguageRenderer.js
@@ -28,7 +28,7 @@ const LanguageRenderer = (props) => {
 			<TextField
 				readOnly={true}
 				variant="outlined"
-				style={{ width: "30vw" }}
+				style={{ width: "25vw" }}
 				value={`${props.language}:  ${props.level}`}
 			/>
 		</div>
