7ee8186f761e5427b02648a01f362214f3cf1658
Donadonzi <donadonzi@gmail.com>
2020-07-21 16:46:34 -0400

    modified snippet routes and matchReviewer()

--- server/utils/match.js
+++ server/utils/match.js
@@ -1,12 +1,19 @@
+const axios = require("axios");
 const express = require("express");
 const User = require("../models/user");
+const Snippet = require("../models/snippet");
 
 const randomIndexGenerator = (length) => {
 	const randomIndex = Math.floor(Math.random() * length);
 	return randomIndex;
 };
 
-const matchReviewer = async (language, level, id) => {
+const matchReviewer = async (snippet_id) => {
+	const snippet = await Snippet.findById(snippet_id);
+	const language = snippet.language;
+	const author = snippet.populate("author").execPopulate();
+	const level = author.experience.get(language);
+
 	let reviewers = await User.find({
 		["experience." + language]: { $gte: level },
 	});
@@ -17,7 +24,7 @@ const matchReviewer = async (language, level, id) => {
 	let reviewer = reviewers[randomIndexGenerator(reviewers.length)];
 
 	while (done === false) {
-		if (reviewer.declined.indexOf(id) != -1) {
+		if (reviewer.declined.indexOf(snippet_id) != -1) {
 			console.log("Declined by: ", reviewer);
 
 			// remove that person from the array
@@ -28,6 +35,8 @@ const matchReviewer = async (language, level, id) => {
 		} else {
 			// send request for the reviewer
 			console.log("Send request to: ", reviewer);
+			await axios.post(`/api/users/request/${reviewer._id}/${snippet_id}`);
+
 			done = true;
 		}
 	}
